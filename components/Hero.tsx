19bbb7241f3bfd17f45251c21fa518a2b8c8d22e
Ifeanyi David Ezechukwukere <130338832+Ifeanyidavid2@users.noreply.github.com>
2025-11-07 01:45:44 -0800

    Update profile image source in Hero component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { SocialIcon } from './Icons';
+import profileImage from '.assets/Me.png';
 
 interface HeroProps {
   scrollToContacts: () => void;
@@ -64,7 +65,7 @@ const Hero: React.FC<HeroProps> = ({ scrollToContacts }) => {
             <div className="relative w-80 h-80 md:w-96 md:h-96">
               <div className="absolute inset-0 bg-gradient-to-br from-indigo-600 to-purple-600 rounded-full transform -rotate-12"></div>
               <img 
-                src="./access/Me.png" 
+                src= {profileImage}
                 alt="Ifeanyi David Ebere Ezechukwukere" 
                 className="relative w-full h-full object-cover rounded-full shadow-2xl border-4 border-gray-800"
               />
